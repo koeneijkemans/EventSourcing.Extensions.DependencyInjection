945e2c2b1a2755249de7052d619b0f3738169b57
Koen Eijkemans <koen.eijkemans@gmail.com>
2021-03-06 11:08:37 +0100

    Initial commit for the extension methods

---
+++ Kei.EventSourcing.Extensions.DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Kei.EventSourcing.ServiceCollectionExtension
+{
+    public static class ServiceCollectionExtension
+    {
+        /// <summary>
+        /// Adds the default setup for event sourcing to the service collection.
+        /// </summary>
+        /// <param name="services">The service collection</param>
+        /// <returns>The extended <see cref="IServiceCollection" /></returns>
+        public static IServiceCollection AddEventSourcing(this IServiceCollection services)
+        {
+            services.AddTransient<StateConnector>();
+            services.AddTransient<CommandHandler>();
+            services.AddTransient<EventPublisher, EventPublisher>();
+
+            // Scan assemblies for classes that implement ICommandHandler
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            foreach (Assembly assembly in assemblies)
+            {
+                var commandHandlerTypes = assembly
+                    .GetTypes()
+                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition().IsAssignableFrom(typeof(ICommandHandler<>))))
+                    .ToList();
+
+                foreach (var commandHandlerType in commandHandlerTypes)
+                {
+                    List<Type> allGenericTypes = commandHandlerType.GetInterfaces().SelectMany(i => i.GetGenericArguments()).ToList();
+
+                    foreach (var genericType in allGenericTypes)
+                    {
+                        var serviceType = typeof(ICommandHandler<>).MakeGenericType(genericType);
+
+                        services.AddTransient(serviceType, commandHandlerType);
+                    }
+                }
+            }
+
+            return services;
+        }
+    }
+}
